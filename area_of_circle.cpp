f5c2a4de54183d1d1d80bd46a647162ecf9337d7
Dadakhalandar S A <dadugkt856775@gmail.com>
2026-05-26 18:04:51 +0530

    Add files via upload

---
+++ area_of_circle.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 2;
+    int y = 4;
+    float r = 10.876;
+    float a = 3.1415*r*r;
+    x++;
+    cout<<x+y<<endl<<x-y<<endl<<x*y<<endl;
+    cout<<"Area is : ";
+    cout<<a<<endl;
+    cout<<x+a<<endl<<y*35343-a*323;
+}
+    
